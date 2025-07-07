85e123f147a6f145cdd1e636acf92266fb44c607
Игорь <sikey21rus@mail.ru>
2025-07-07 16:07:57 +0300

    Update send_telegram_image.R

--- Scripts/send_telegram_image.R
+++ Scripts/send_telegram_image.R
@@ -2,7 +2,6 @@
 send_telegram_image <- function(bot_token = Sys.getenv("TELEGRAM_TOKEN"),
                                 chat_id = Sys.getenv("TELEGRAM_CHAT_ID"), 
                                 directory = "output/") {
-  
   image_path <- get_latest_file(directory)
   
   if (is.null(image_path)) {
@@ -11,18 +10,24 @@ send_telegram_image <- function(bot_token = Sys.getenv("TELEGRAM_TOKEN"),
   }
   
   url <- paste0("https://api.telegram.org/bot", bot_token, "/sendPhoto")
-  res <- POST(url, 
+  
+  tryCatch({
+    res <- httr::POST(url, 
               body = list(
                 chat_id = chat_id,
-                photo = upload_file(image_path)
+                photo = httr::upload_file(image_path)
               ), 
               encode = "multipart")
-  
-  if (status_code(res) == 200) {
-    message("Изображение успешно отправлено в Telegram.")
-    return(TRUE)
-  } else {
-    warning("Ошибка при отправке изображения в Telegram: ", content(res, "text"))
+    
+    if (httr::status_code(res) == 200) {
+      message("Изображение успешно отправлено в Telegram.")
+      return(TRUE)
+    } else {
+      warning("Ошибка при отправке изображения в Telegram: ", httr::content(res, "text"))
+      return(FALSE)
+    }
+  }, error = function(e) {
+    warning("Ошибка при попытке отправить изображение: ", e$message)
     return(FALSE)
-  }
+  })
 }
