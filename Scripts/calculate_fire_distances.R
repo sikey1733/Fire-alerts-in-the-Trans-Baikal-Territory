4eb5571eb958cc74a12621dd2ea5f25dc7003a2d
Игорь <sikey21rus@mail.ru>
2025-07-10 21:45:45 +0300

    Update calculate_fire_distances.R

--- Scripts/calculate_fire_distances.R
+++ Scripts/calculate_fire_distances.R
@@ -1,7 +1,7 @@
 calculate_fire_distances <- function(region_names,
-                                     csv_data_dir = "data/",
-                                     places_cache = "data/places.gpkg",
-                                     water_cache = "data/waterbodies.gpkg") {
+                                    csv_data_dir = "data/",
+                                    places_cache = "data/places.gpkg",
+                                    water_cache = "data/waterbodies.gpkg") {
   fire_df <- read_data_viirs(csv_data_dir)
   if (is.null(fire_df)) {
     message("Нет данных о пожарах.")
@@ -22,7 +22,6 @@ calculate_fire_distances <- function(region_names,
     return(NULL)
   }
   
-  # Преобразуем полигоны водоемов к центроидам
   water_points <- st_centroid(water_sf)
   
   # Расстояния
@@ -33,14 +32,11 @@ calculate_fire_distances <- function(region_names,
   nearest_place_indices <- apply(dist_places, 1, which.min)
   nearest_place_names <- places_sf$name[nearest_place_indices]
   
-  # Добавляем region_name ближайшего населённого пункта
-  nearest_region_names <- places_sf$region_name[nearest_place_indices]
-  
   min_water_dists <- apply(dist_water, 1, min)
   
   fires_sf$distance_to_settlement_km <- round(as.numeric(min_place_dists) / 1000, 2)
   fires_sf$settlement_name <- nearest_place_names
-  fires_sf$settlement_region <- nearest_region_names  # Новое поле с регионом
+  
   fires_sf$distance_to_water_km <- round(as.numeric(min_water_dists) / 1000, 2)
   
   message("Готово! Найдено точек: ", nrow(fires_sf))
