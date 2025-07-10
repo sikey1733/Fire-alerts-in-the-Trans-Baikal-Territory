05ed810c2252360b089232a5a6f0a5ac14f90394
Игорь <sikey21rus@mail.ru>
2025-07-10 20:21:53 +0300

    Update pipeline.R

--- Scripts/pipeline.R
+++ Scripts/pipeline.R
@@ -11,7 +11,7 @@ main <- function() {
   required_packages <- c(
     "magrittr", "dplyr", "ecmwfr", "stars", "sf", "units",
     "lubridate", "httr", "geosphere", "osmdata", "ggplot2",
-    "ggrepel", "ggspatial"
+    "ggrepel", "ggspatial", "ggmap"
   )
 
   install_and_load <- function(pkg) {
