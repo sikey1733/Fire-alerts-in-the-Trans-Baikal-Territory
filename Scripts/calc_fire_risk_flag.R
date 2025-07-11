9b64f1489fb7626122985451ba542e6b941853d6
Игорь <sikey21rus@mail.ru>
2025-07-11 12:48:48 +0300

    Update calc_fire_risk_flag.R

--- Scripts/calc_fire_risk_flag.R
+++ Scripts/calc_fire_risk_flag.R
@@ -1,7 +1,8 @@
 # Функция для вычисления флага риска по данным за 1 день
 calc_fire_risk_flag <- function(day_df) {
   required_cols <- c("temperature_air_C", "total_precip_mm", "wind_speed_m", 
-                     "soil_temperature_level_1_C", "soil_temperature_level_2_C")
+                     "soil_temperature_level_1_C", "soil_temperature_level_2_C",
+                    "leaf_index_low_vegetation_m_2_m_2")
   
   if (is.null(day_df) || nrow(day_df) == 0) {
     warning("Данные за день отсутствуют или пусты")
@@ -17,10 +18,10 @@ calc_fire_risk_flag <- function(day_df) {
   mean_temp <- mean(day_df$temperature_air_C, na.rm = TRUE)
   sum_precip <- max(day_df$total_precip_mm, na.rm = TRUE)
   if (is.na(sum_precip)) sum_precip <- 0
-  
   mean_wind <- mean(day_df$wind_speed_m, na.rm = TRUE)
   stl1 <- mean(day_df$soil_temperature_level_1_C, na.rm = TRUE)
   stl2 <- mean(day_df$soil_temperature_level_2_C, na.rm = TRUE)
+  leaf_index <- mean(day_df$leaf_index_low_vegetation_m_2_m_2, na.rm = TRUE)
   
   if (mean_temp >= 15 && sum_precip < 10 && mean_wind >= 3 && stl1 >= 5 && stl2 >= 5) {
     return("Высокий риск")
